623a80f40b80d5fc1b5860232a20d03aec9699a1
BramLeisink <bramleisink@outlook.com>
2023-08-26 07:56:21 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,30 @@
+import json
+
+
+def generate_edges():
+    num_faces = int(input("Enter the number of faces: "))
+    faces = {}
+    edges = []
+
+    for face in range(1, num_faces + 1):
+        faces[face] = []
+
+    for face in range(1, num_faces+1):
+        print(f"---FACE {face}---")
+        for connection in faces[face]:
+            print(f"Already connected to: {connection}")
+        while True:
+            inpt = input("Enter a new connection (or next for next face): ")
+
+            try:
+                faces[face].append(int(inpt))
+                faces[int(inpt)].append(face)
+                edges.append([face, int(inpt)])
+            except:
+                break
+    print(faces)
+    print(edges)
+
+
+if __name__ == "__main__":
+    generate_edges()
